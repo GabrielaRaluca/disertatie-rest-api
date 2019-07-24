021a4b5572312e961a067557dbe1405b18acd081
Raluca Buturca <gabriela.br141995@gmail.com>
2019-07-24 11:24:54 +0300

    first app schema

---
+++ src/main/java/com/dissertation/restapi/model/User.java
@@ -0,0 +1,26 @@
+package com.dissertation.restapi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    String email;
+
+    @Column(nullable =  false)
+    String name;
+}
